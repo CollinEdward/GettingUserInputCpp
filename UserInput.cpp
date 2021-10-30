57692d5d988b5ef9e66433240b0d705cd3e3e9e1
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2021-10-30 15:24:19 +0000

    Added Comments

--- UserInput.cpp
+++ UserInput.cpp
@@ -4,9 +4,11 @@ using namespacestd;
 
 int main() {
     int x;
+    // This just prints out "type your age" in the terminal
     cout << "Type your age: ";
-    cin >> x;
+    cin >> x; // cin is just used to accept the input from the standard input device; keyboard
     cout << "Your age is: " << x; 
+    // prints out the age, x is the age.
 
 
 }
